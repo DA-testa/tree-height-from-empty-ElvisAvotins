63ffdb724492080a07be0fab975b407193e3b4d3
ElvisAvotins <elvis.avotins@edu.rtu.lv>
2023-03-04 15:35:13 +0200

    2. version

--- tree_height.py
+++ tree_height.py
@@ -2,17 +2,31 @@
 
 import sys
 import threading
-import numpy
+#import numpy
 
 
-def compute_height(n, parents):
+def compute_height(n, nodes):
     # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    levels = [0] * n
+    for i in range(n):
+        parent = nodes[i]
+        if parent == -1:
+            levels[i] = 1
+        else:
+            levels[i] = levels[parent] + 1
 
+
+    return max(levels)
+    # Your code here
 
 def main():
+    size = int(input())
+    arr = list(map(int, input().split()))
+    max_depth = compute_height(size, arr)
+    print(max_depth)
+
+if __name__ == '__main__':
+    main()
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
