0c8221bb77546ce15b5cc5346421bcbea2f1318f
ElvisAvotins <elvis.avotins@edu.rtu.lv>
2023-03-04 19:05:15 +0200

    commit

--- tree_height.py
+++ tree_height.py
@@ -1,12 +1,10 @@
-# python 3
+# python3
 import os
 import sys
 import threading
-import numpy as np 
-
-
+#import numpy
 def compute_height(n, nodes):
-    levels = np.zeros(n, dtype=np.int32)
+    levels = [0] * n
     root = -1
     for i in range(n):
         if nodes[i] == -1:
@@ -24,37 +22,36 @@ def compute_height(n, nodes):
             if nodes[i] == node:
                 stack.append(i)
 
-    return np.max(levels)
+    return max(levels)
 
-
-def main():
+def main(): 
+    # implement input form keyboard and from files
+    # let user input file name to use, don't allow file names with letter a
+    # account for github input inprecision
     script_dir = os.path.dirname(__file__)
+    # print(script_dir)
     while True:
         file_or_input = input().strip().lower()
-        arr = np.array([], dtype=np.int32)
+        size = int(0) 
+        arr = []
         if file_or_input == "i":
             size = int(input())
-            arr = np.fromstring(input().strip(), sep=' ', dtype=np.int32)
-            max_depth = compute_height(size, arr)
-            print(max_depth)
+            arr = list(map(int, input().split()))
         if file_or_input == "f":
-            file_name = input()
+            file_name = (input())
             if 'a' in file_name:
                 break
-            file_path = os.path.join(script_dir, "test", file_name)
+            file_path = (script_dir + "/test/" + file_name)
             with open(file_path) as f:
                 size = int(f.readline())
-                arr = np.fromstring(f.readline().strip(), sep=' ', dtype=np.int32)
-            max_depth = compute_height(size, arr)
-            print(max_depth)
-        else:
-            break
-    pass
-
+                arr = list(map(int, f.readline().strip().split()))
+        max_depth = compute_height(size, arr)
+        print(max_depth)
+        pass
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+# main()
